b3378297d972539fb42709cfcd51189e574bb1fb
david.didnik <david.didnik@leplusduweb.com>
2024-05-22 14:00:45 +0200

    [feat] : Optimize mobiles API endpoint with caching

--- src/Controller/MobileController.php
+++ src/Controller/MobileController.php
@@ -4,23 +4,41 @@ namespace App\Controller;
 
 use App\Entity\Mobile;
 use App\Repository\MobileRepository;
+use JMS\Serializer\SerializationContext;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class MobileController extends AbstractController
 {
     #[Route('/api/mobiles', name: 'app_mobile', methods: ['GET'])]
-    public function index(Request $request, MobileRepository $mobileRepository, SerializerInterface $serializer): JsonResponse
-    {
+    public function index(
+        Request $request,
+        MobileRepository $mobileRepository,
+        SerializerInterface $serializer,
+        TagAwareCacheInterface $cache
+    ): JsonResponse {
         $page = $request->get('page', 1);
         $limit = $request->get('limit', 10);
 
-        $mobiles = $mobileRepository->paginateMobiles($page, $limit);
-        $jsonMobiles = $serializer->serialize($mobiles, 'json');
+        $idCache = "allMobiles-" . $page . "-" . $limit;
+
+        $jsonMobiles = $cache->get(
+            $idCache,
+            function (ItemInterface $item) use ($mobileRepository, $page, $limit, $serializer) {
+                $item->tag("mobilesCache");
+                $item->expiresAfter(86400);
+                $context = SerializationContext::create();
+                $bookList = $mobileRepository->paginateMobiles($page, $limit);
+
+                return $serializer->serialize($bookList, 'json', $context);
+            }
+        );
 
         return new JsonResponse($jsonMobiles, Response::HTTP_OK, [], true);
     }
