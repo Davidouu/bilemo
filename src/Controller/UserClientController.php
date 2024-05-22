9a6d9a4dcaf0fed31c9e278164575370137e7f80
david.didnik <david.didnik@leplusduweb.com>
2024-05-22 08:51:28 +0200

    [feat] : add create userClient route

--- src/Controller/UserClientController.php
+++ src/Controller/UserClientController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\UserClient;
 use App\Repository\UserClientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserClientController extends AbstractController
 {
@@ -31,5 +35,28 @@ class UserClientController extends AbstractController
         $jsonUserClient = $serializer->serialize($userClient, 'json');
         
         return new JsonResponse($jsonUserClient, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/clients', name: 'app_user_client_create', methods: ['POST'])]
+    public function create(Request $request, SerializerInterface $serializer, ValidatorInterface $validator, EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+        $userClient = $serializer->deserialize($request->getContent(), UserClient::class, 'json');
+
+        $errors = $validator->validate($userClient);
+
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        $userClient->setUser($this->getUser());
+
+        $entityManager->persist($userClient);
+        $entityManager->flush();
+
+        $jsonUserClient = $serializer->serialize($userClient, 'json');
+        
+        $location = $urlGenerator->generate('app_user_client_show', ['id' => $userClient->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonUserClient, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 }
